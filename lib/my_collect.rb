e211f749a9ca1a78ec472cf6c636f86aecbf7779
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-16 15:25:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(array)
+  i = 0
+  collection = []
+ # languages = ["ruby", "javascript", "python", "objective-c"]
+  #students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+  my_collect(array) do |name|
+  name.split(" ").first
+   # language.upcase
+  end
+ # my_collect
+end
 
 
+
+#def my_collect(empty_array)
+  #i = 0
+ # collection = []
+  #while i < array.length
+  #collection << yield(empty_array[i])
+ # my_collect(empty_array) do |x|
+   # i += 1
+  #end
+  #my_collect
+#end
